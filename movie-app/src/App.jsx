29cfce2e5996fd5224c9bff45b3af0a274571295
vyshnav-u <vyshnavuvinay@gmail.com>
2026-08-14 00:13:04 +0530

    Add movie app image change

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -3,10 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Header from '../components/Header'
-import About from '../components/About'
-import Movie from '../components/Movie'
-import Footer from '../components/Footer'
+import Header from './components/Header'
+import About from './components/About'
+import Movie from './components/Movie'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
--- movie-app/src/components/About.jsx
+++ movie-app/src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import heart from '../assets/heart.png'
 
 function About() {
     return (
@@ -9,7 +10,7 @@ function About() {
                 <div className='flex items-center justify-between relative px-9 p-9'>
                     <h1 className=' hidden md:block bebas md:text-[200px] max-w-[800px] md:leading-[150px] text-white'>WATCH THE BEST SHOWS*</h1>
                     <h1 className=' bebas text-right text-[100px] md:text-[200px] max-w-[800px] leading-[90px] md:leading-[150px] text-white/50'>Watch Enjoy Discover </h1>
-                    <img className='h-[250px] md:h-[400px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20' src="./src/assets/heart.png" alt="" />
+                    <img className='h-[250px] md:h-[400px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20' src={heart} alt="" />
                 </div>
                 <p className=' px-9 space italic leading-[16px] text-xs text-right pb-5 text-white'>**Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente minus totam est perferendis possimus mollitia inventore, reprehenderit culpa dicta temporibus animi qui aliquid iste illum suscipit a quo necessitatibus nisi!</p>
 
